bcd7cfa460c257b290a6e19788142af2b88d49bf
Armando <jaimes087.uz@gmail.com>
2020-07-11 12:41:58 -0500

    Updates

---
+++ precache-manifest.935ccc2eaddebb0d195cbab7d2348d31.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826fc447999e665d149120c34edc084f",
+    "url": "/react-triprise-landing/index.html"
+  },
+  {
+    "revision": "ee7d003c57aff4908f9d",
+    "url": "/react-triprise-landing/static/css/main.f9416c2a.chunk.css"
+  },
+  {
+    "revision": "4500b4245c4bc0bceeb2",
+    "url": "/react-triprise-landing/static/js/2.f0c38b16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-triprise-landing/static/js/2.f0c38b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee7d003c57aff4908f9d",
+    "url": "/react-triprise-landing/static/js/main.d721e4a1.chunk.js"
+  },
+  {
+    "revision": "3c5c9224fb94a83fec80",
+    "url": "/react-triprise-landing/static/js/runtime-main.1ac23956.js"
+  },
+  {
+    "revision": "296a73327b580df80389272ea82e8528",
+    "url": "/react-triprise-landing/static/media/abu-dhabi.296a7332.jpg"
+  },
+  {
+    "revision": "8ac7456f43b6ed4da153855dc8e49886",
+    "url": "/react-triprise-landing/static/media/australia.8ac7456f.jpg"
+  },
+  {
+    "revision": "aa1a2807f83497c356999d3902f6f491",
+    "url": "/react-triprise-landing/static/media/hero.aa1a2807.jpg"
+  },
+  {
+    "revision": "22b7b0bdf47219eabcaec5100b06ea7a",
+    "url": "/react-triprise-landing/static/media/offer.22b7b0bd.jpg"
+  },
+  {
+    "revision": "7dab95de5c175c88c682b6a8c7e242b3",
+    "url": "/react-triprise-landing/static/media/paris.7dab95de.jpg"
+  },
+  {
+    "revision": "42b76ba562e62b8a239bb908070bafcd",
+    "url": "/react-triprise-landing/static/media/popular1.42b76ba5.jpg"
+  },
+  {
+    "revision": "482bc1a6dd59a64091cf94f8e11f452c",
+    "url": "/react-triprise-landing/static/media/popular2.482bc1a6.jpg"
+  },
+  {
+    "revision": "6a8c7d440030b8285c25824dc9a0f060",
+    "url": "/react-triprise-landing/static/media/popular3.6a8c7d44.jpg"
+  },
+  {
+    "revision": "730d887f85009ce88e9b72e57d5b9e6b",
+    "url": "/react-triprise-landing/static/media/popular4.730d887f.jpg"
+  },
+  {
+    "revision": "d8d8ee6d1d0411997dc13a6272ab6969",
+    "url": "/react-triprise-landing/static/media/popular5.d8d8ee6d.jpg"
+  }
+]);
